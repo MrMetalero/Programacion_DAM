e3bd7898673c2f6fe2f1da515a56644bf45595c6
Mr_Metalero <danymena7@gmail.com>
2023-09-20 17:58:59 +0200

    cambio

---
+++ ejercicio1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ejercicio1 {
+    
+
+public static void main(String[] args){
+
+int numero1;
+int numero2;
+
+
+Scanner sc= new Scanner(System.in);
+
+System.out.println("introduce un número");
+    numero1= sc.nextInt();
+
+System.out.println("introduce un segundo número");
+    numero2= sc.nextInt();
+
+    if (numero1 > numero2) {
+        System.out.println (numero1);
+
+    } else {System.out.println(numero2);}
+
+
+
+
+}
+
+
+}
