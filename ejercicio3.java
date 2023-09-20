a5daf62666f2dbf10e98149a80463b4d4e528f35
Mr_Metalero <danymena7@gmail.com>
2023-09-20 17:29:25 +0200

    cambio1

---
+++ ejercicio3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; //IMPORTATE EN CADA SCRIPT ME DA IGUAL
+
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int suma = 10;
+        int numeroint1;
+
+          
+
+        for (int i = 1;i <= 10;i++) {
+            System.out.println("Introduce un número: ");
+            numeroint1 = sc.nextInt();
+            suma = numeroint1 + suma;
+        }
+
+        System.out.println(suma);
+    }
+
+}
