1f3f09494206be08ca3509d710ddbc7ce408b681
Mr_Metalero <danymena7@gmail.com>
2023-09-22 17:38:17 +0200

    update ejercicio13.java

--- ejercicio13.java
+++ ejercicio13.java
@@ -11,19 +11,29 @@ public class ejercicio13 {
     static double F_Primos(double numX){
         double numY = 1;
         int contador = 2;
-        System.out.println("Dentro de la función");
-        System.out.println("numX o i será ahora" + numX);
+        //System.out.println("Dentro de la función"); para debug
         double resto;
         boolean esprimo = true;
 
-    if(numX < 2){
-        System.out.println("El número o es primo");
-    }else if(numX > 4){
+    if(numX < 2){ //elimina 1 y negativos, no son primos
+        System.out.println("---El número  no es primo---");
+    }else if(numX >= 4){ //comprueba cualquier número a partir de 4 
             
-            do resto = numX % 1; while( esprimo && contador < numX);
-            if(esprimo){System.out.println("El número es primo");}
+            do {resto = numX % contador; System.out.println("comprobando... "+ numX + " dividido entre " + contador); contador++;System.out.println("El divisor es ahora...: " + contador);}
+            while( esprimo && contador < numX && resto != 0 ); //asigna a resto el modulo de X entre
+           
+            /*  Si sale del loop  porque su resto es 0 en cualquiera de los pasos, no es primo 
+             * menos cuando numX = contador, que es cuando se divide entre si mismo, cosa que
+             * es una propiedad de los primos
+            */
 
-        }
+            if(resto == 0){
+                System.out.println("---El número no es primo---"); 
+
+
+            }else  System.out.println("---El número es primo---");
+            
+        }else System.out.println("---El número es primo---"); //contempla 2 y 3, primos
 
         return 0;
     }
@@ -39,10 +49,9 @@ public class ejercicio13 {
         num1 = sc.nextDouble();
 
 
-        if(num1 != 0 && num1 > 1){
-            System.out.println("Dentro del if en principal");
+        if(num1 != 0 && num1 > 1){ 
             F_Primos(num1);
-            System.out.println("Al finalizar la función");
+    
         }
 
 
