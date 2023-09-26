e6c7d411c47cc6f862e2fdc692653a37e6180cb7
Mr_Metalero <danymena7@gmail.com>
2023-09-26 19:34:01 +0200

    update Ejercicio2.java

---
+++ ejerciciosresueltos/Ejercicio3.java
@@ -0,0 +1,38 @@
+package ejerciciosresueltos;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    
+
+
+
+
+
+
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+        int b;
+        int variableTraspaso;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println("Variable 'a' introducida inicial: "+ a);
+        System.out.println("Variable 'b' introducida inicial: "+ b);
+        variableTraspaso = a;
+        a = b;
+        b = variableTraspaso;
+
+        System.out.println("La variable a = "+ a + "la variable b = " + b);
+        
+
+
+    }
+
+
+
+
+
+}
