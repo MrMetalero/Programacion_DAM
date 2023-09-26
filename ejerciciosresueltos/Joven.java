157c115ef6f317b53d34ed22d6f789819489ee6a
Mr_Metalero <danymena7@gmail.com>
2023-09-26 19:57:14 +0200

    dqdfr2r

---
+++ ejerciciosresueltos/Joven.java
@@ -0,0 +1,24 @@
+package ejerciciosresueltos;
+import java.util.Scanner;
+
+public class Joven {
+    
+
+    public static void main(String[]args){
+
+        int edad =18 , nivelDeEstudios = 1, ingresos =18999;
+        boolean jasp = false;
+
+        if(edad <=28 && nivelDeEstudios > 3 && ingresos > 28000){
+            jasp = true;
+        }
+
+
+
+
+    }
+
+
+
+
+}
