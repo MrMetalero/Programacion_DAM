9780095512c36c3157a542ea76b8044eceb77bef
MrMetalero <danymena7@gmail.com>
2023-09-26 12:17:28 +0200

    update ejercicio13.java adq21

---
+++ supuestopractico1/supuestoPrimero.java
@@ -0,0 +1,26 @@
+package supuestopractico1;
+
+public class supuestoPrimero {
+ 
+
+
+public static void main(String[]args){
+
+final int CONS_SUPUESTO_1 = 1008;
+
+final String CONS_SUPUESTO_2 = "Saludos";
+
+
+Integer variableWrapper1 = CONS_SUPUESTO_1;
+
+Integer variableWrapper2 = variableWrapper1;
+
+System.out.println(variableWrapper1 + " " + variableWrapper2 + " " + CONS_SUPUESTO_2);
+
+
+}
+
+
+
+}
+
