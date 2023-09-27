5145feb3fd5efee6d7ccf9baa277d4afc4fe0c7f
Mr_Metalero <danymena7@gmail.com>
2023-09-27 17:19:02 +0200

    update Ejercicio2.java

--- boletin_desde_cero/Ejercicio2.java
+++ boletin_desde_cero/Ejercicio2.java
@@ -12,16 +12,29 @@ public class Ejercicio2 {
         int X = 1, Y = 2;
         double N = 1.1, M = 1.2;
 
-        
+        System.out.println("X + N:");
         System.out.println(X+N);
+
+        System.out.println("X * N:");
         System.out.println(X*N);        
         
+        System.out.println("Y + M:");
         System.out.println(Y+M);
+
+        System.out.println("Y * M:");
         System.out.println(Y*M);
 
+        System.out.println("X - N:");
         System.out.println(X-N);
 
+        System.out.println("Y - M:");
         System.out.println(Y-M);
+
+        System.out.println("X / N:");
+        System.out.println(X/N);
+
+        System.out.println("Y / M:");
+        System.out.println(Y/M);
 
 
 
