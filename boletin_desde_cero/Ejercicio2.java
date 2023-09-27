4649242fb62140250c0609a9203c8749aeaa5a4c
MrMetalero <danymena7@gmail.com>
2023-09-27 14:46:46 +0200

    1241º542r

---
+++ boletin_desde_cero/Ejercicio2.java
@@ -0,0 +1,37 @@
+package boletin_desde_cero;
+import java.util.*;
+import java.math.*;
+
+
+public class Ejercicio2 {
+
+  
+
+
+    public static void main(String[]args){
+        int X = 1, Y = 2;
+        double N = 1.1, M = 1.2;
+
+        
+        System.out.println(X+N);
+        System.out.println(X*N);        
+        
+        System.out.println(Y+M);
+        System.out.println(Y*M);
+
+        System.out.println(X-N);
+
+        System.out.println(Y-M);
+
+
+
+    }
+    
+    
+
+
+
+
+
+
+}
