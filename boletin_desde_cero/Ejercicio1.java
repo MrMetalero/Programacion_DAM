b63195d7bccbc634521f3c2d7139abb7cb3afdec
MrMetalero <danymena7@gmail.com>
2023-09-27 14:23:06 +0200

    update ejercicio6.java

---
+++ boletin_desde_cero/Ejercicio1.java
@@ -0,0 +1,41 @@
+package boletin_desde_cero;
+import java.util.*;
+
+
+
+public class Ejercicio1 {
+
+
+
+    
+    public static void main(String[]args){
+        
+    int N = 8;
+    double A = 9.5;
+    char C = 'a';
+
+    System.out.println(+ N + A + C);
+   
+    System.out.println("Suma de N + A: "+(N+A)+"\n" 
+                    + "La resta de N - A: "+ (N-A)+"\n"
+                    + "El valor numérico de 'a' es : "+ (Character.getNumericValue(C)) 
+    );
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
