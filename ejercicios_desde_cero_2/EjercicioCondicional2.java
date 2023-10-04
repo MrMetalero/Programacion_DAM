5d709183792dcd1ace4d88e636a78786c9dc76eb
MrMetalero <danymena7@gmail.com>
2023-10-04 14:54:40 +0200

    rr2r2r2

---
+++ ejercicios_desde_cero_2/EjercicioCondicional2.java
@@ -0,0 +1,36 @@
+package ejercicios_desde_cero_2;
+import java.util.Scanner;
+import java.math.*;
+
+public class EjercicioCondicional2 {
+    
+    public static void main(String[] args) {
+        int numEntero;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce un número");
+        numEntero = sc.nextInt();
+
+
+        if (numEntero %10 == 0) {
+            System.out.println("El número es múlitplo de 10");
+        } else{
+            System.out.println("El número NO es múlitplo de 10");
+        }
+        
+        
+        
+
+        
+
+            
+
+
+
+
+
+    }
+
+
+
+}
---
+++ ejercicios_desde_cero_2/EjercicioCondicional3.java
@@ -0,0 +1,31 @@
+package ejercicios_desde_cero_2;
+import java.util.Scanner;
+
+public class EjercicioCondicional3 {
+    
+
+    public static void main(String[] args) {
+        String input = "";
+        String input2 = "";        
+        Scanner sc = new Scanner(System.in);
+
+
+        if (input.) {
+            
+        } else {
+            
+        }
+
+        input = sc.nextLine();
+        input2 = sc.nextLine();
+
+
+
+
+
+
+
+    }
+
+
+}
