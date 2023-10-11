ea409b0f011e25a3596896063fdf4124bfaca2ce
MrMetalero <danymena7@gmail.com>
2023-10-11 14:41:22 +0200

    d1d1d1

---
+++ ejercicios_adicionales_t5/Ejercicio2.java
@@ -0,0 +1,10 @@
+package ejercicios_adicionales_t5;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+
+
+
+    }
+}
---
+++ ejercicios_adicionales_t5/Ejercicio3.java
@@ -0,0 +1,22 @@
+package ejercicios_adicionales_t5;
+
+public class Ejercicio3 {
+    
+    public static void main(String[] args) {
+        
+        int controlNumeros100 = 0;
+        
+
+        while (controlNumeros100 < 100) {
+            if (controlNumeros100 % 2 == 0 || controlNumeros100 % 3 == 0) {
+                System.out.println(controlNumeros100);
+            } 
+            controlNumeros100 += 1;
+        }
+       
+
+
+
+
+    }
+}
