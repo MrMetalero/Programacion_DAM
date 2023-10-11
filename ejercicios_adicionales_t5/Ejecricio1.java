eaaa9c040a141dae4143ce3c0b3f27f074191c15
MrMetalero <danymena7@gmail.com>
2023-10-11 13:39:53 +0200

    chore: update launch.json

---
+++ ejercicios_adicionales_t5/Ejecricio1.java
@@ -0,0 +1,12 @@
+package ejercicios_adicionales_t5;
+
+public class Ejecricio1 {
+    public static void main(String[] args) {
+        int sumatotal;
+        int controlIntroducirDatos = 0;
+
+
+
+
+    }    
+}
