0ac78d7b528663460a6be3808ec744b0d3164b8c
MrMetalero <danymena7@gmail.com>
2023-10-17 12:13:04 +0200

    update Ejercicio4.java d1dd

--- ejercicios_adicionales_t5/Ejercicio4.java
+++ ejercicios_adicionales_t5/Ejercicio4.java
@@ -7,15 +7,34 @@ public class Ejercicio4 {
         String password = "pass";
         String inputComparador = "";
         Scanner sc = new Scanner(System.in);
+        int loggedIn = 0;
 
 
 
-
+        System.out.println("Introduce la contraseña para entrar");
         for (int i = 0; i < 3; i++) {
-            
-            
+
+            if(loggedIn == 0){
+
+                inputComparador = args[0];
+
+                    
+                if (inputComparador.equals(password)) {
+                    System.out.println("Bienvenido");
+                    loggedIn = 1;
+
+                } else {
+                    System.out.println("Por favor inténtalo de nuevo " + (i+1));
+                }
+
+            }else{
 
 
+
+
+
+
+            }
 
 
 
