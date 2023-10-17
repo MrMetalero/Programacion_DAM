25c1a69bceb5eda3cc10dce178e8e81bee846f00
MrMetalero <danymena7@gmail.com>
2023-10-17 13:03:50 +0200

    update Ejercicio6.java

--- ejercicios_adicionales_t5/Ejercicio6.java
+++ ejercicios_adicionales_t5/Ejercicio6.java
@@ -1,13 +1,23 @@
 package ejercicios_adicionales_t5;
 
 public class Ejercicio6 {
+
+    //funciona
     public static void main(String[] args) {
-    int numRandomizado = (int)((Math.random() *100)+1);        
+
+        for (int i = 0; i < 10; i++) {
+            int numRandomizado = (int)((Math.random() *100)+1);
+            System.out.println(numRandomizado);
+        }
+
+
+
+        
 
   
 
 
     }
 
-    
+
 }
