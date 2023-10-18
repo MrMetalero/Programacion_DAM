077835bd531da216011fbcd3cc0fb8231ea6f88d
MrMetalero <danymena7@gmail.com>
2023-10-18 14:24:26 +0200

    212f2

---
+++ ejercicios_adicionales_t5/Ejercicio10.java
@@ -0,0 +1,29 @@
+package ejercicios_adicionales_t5;
+import java.util.Scanner;
+import java.math.*;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int numInput;
+        Scanner sc = new Scanner(System.in);
+
+
+        for(int i = 0; i < 10; i++){
+
+        
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
