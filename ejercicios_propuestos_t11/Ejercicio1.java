39ec432bebdd50ed0c987055a7a62d34b0001299
Mr_Metalero <danymena7@gmail.com>
2023-10-22 20:46:34 +0200

    egsdgsg

---
+++ ejercicios_propuestos_t11/Ejercicio1.java
@@ -0,0 +1,30 @@
+package ejercicios_propuestos_t11;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static int funcionIterativaFactor( int numRecibido){
+        int factorRetorno = 1;
+    
+
+        
+
+
+        return factorRetorno;
+    }
+
+
+
+
+
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numInputIterativo = sc.nextInt();
+        
+        
+        funcionIterativaFactor(numInputIterativo);
+
+    }
+}
