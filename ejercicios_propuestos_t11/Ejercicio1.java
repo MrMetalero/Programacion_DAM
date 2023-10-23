03819d4d36f5aad2c8138604245b1668d8cf7464
MrMetalero <danymena7@gmail.com>
2023-10-23 14:51:07 +0200

    update launch.json and Ejercicio1.java

--- ejercicios_propuestos_t11/Ejercicio1.java
+++ ejercicios_propuestos_t11/Ejercicio1.java
@@ -4,27 +4,30 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
     public static int funcionIterativaFactor( int numRecibido){
-        int factorRetorno = 1;
+        int factorRetornoResultado = 1;
     
+        //5 = 5*4*3*2*1
+        for (int i = 0; i < numRecibido; i++) {
+            factorRetornoResultado += factorRetornoResultado * i;
 
+            System.out.println("control factorretorno: "+ factorRetornoResultado);
+            
+        }
+
+        return factorRetornoResultado;
         
-
-
-        return factorRetorno;
     }
 
-
-
-
-
-
-    
+        
+      
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un valor para calcular factorial");
         int numInputIterativo = sc.nextInt();
         
         
-        funcionIterativaFactor(numInputIterativo);
+        System.out.println(funcionIterativaFactor(numInputIterativo)); 
 
     }
 }
