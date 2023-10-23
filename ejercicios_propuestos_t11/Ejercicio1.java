d09599228689882a9b6c92312ab98e85aac360f5
MrMetalero <danymena7@gmail.com>
2023-10-23 14:57:07 +0200

    update Ejercicio1.java

--- ejercicios_propuestos_t11/Ejercicio1.java
+++ ejercicios_propuestos_t11/Ejercicio1.java
@@ -3,20 +3,31 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
 
-    public static int funcionIterativaFactor( int numRecibido){
+    public static int funcionIterativaFactor(int numRecibido){
         int factorRetornoResultado = 1;
     
         //5 = 5*4*3*2*1
         for (int i = 0; i < numRecibido; i++) {
             factorRetornoResultado += factorRetornoResultado * i;
 
-            System.out.println("control factorretorno: "+ factorRetornoResultado);
-            
+           
         }
 
         return factorRetornoResultado;
         
     }
+
+    public static int funcionRecursivaFactor(int numRecibido){
+
+
+
+
+
+    return 0;
+
+    }
+
+
 
         
       
@@ -28,6 +39,7 @@ public class Ejercicio1 {
         
         
         System.out.println(funcionIterativaFactor(numInputIterativo)); 
+
 
     }
 }
