8e6b89f8f60e6997eb6e272069cbf5986f6ccd1c
MrMetalero <danymena7@gmail.com>
2023-10-25 14:42:21 +0200

    update Ejercicio9.java and Ejercicio1.java

--- ejercicios_adicionales_t5/Ejercicio9.java
+++ ejercicios_adicionales_t5/Ejercicio9.java
@@ -6,7 +6,7 @@ public class Ejercicio9 {
     public static void main(String[] args) {
         int numeroMax;  
         int controlDeLineas = 1;
-        int filaCompletada = 0;
+        int controlNumPorFila = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Introduce un número límite");
@@ -15,23 +15,18 @@ public class Ejercicio9 {
 
     
             
-        for (int x = 1; x < numeroMax+1; x = x) { //Hacer esto 15 veces por ejemplo
+        for (int x = 1; x < numeroMax+1; x++) { //Hacer esto 15 veces por ejemplo
+            System.out.print(x +" ");
+            controlNumPorFila++;
             
-            
-            for (int i = 0; i < controlDeLineas; i++) {
-            	System.out.print(x);
-                if(x >1){System.out.print(" ");} //Para añadir un espacio a los números y no verlos apelotonados, solo después de imprimir el primero
-                                     //Printea el número una vez en la primera fila
-                                    //Luego al salir del loop le añadimos 1 bien para la siguiente fila
-                                    //O bien para el siguiente print
-                                    //En el caso del 2, lo imprimirá al acabar
-            	
-                if (i == controlDeLineas-1) {
-                    System.out.print("\n");
-                }
-                x += 1;
+            if(controlNumPorFila == controlDeLineas){
+                System.out.print("\n");
+                controlNumPorFila = 0;
+
+                controlDeLineas++;
             }
-            controlDeLineas +=1;
+
+          
         }
 
       
@@ -43,3 +38,38 @@ public class Ejercicio9 {
 
     }
 }
+
+
+
+
+/*
+
+    int = input
+    int = númeroAImprimir es  = i
+    int = numeroHastaSalto = 1
+
+    for(int i = 0 ; i < input; i++){
+
+
+        i
+        
+        if(i == numeroHastaSalto && numeroHastaSalto >){
+            sout(\n)
+            
+
+        }
+        numeroHastaSalto +=1
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
+ */
