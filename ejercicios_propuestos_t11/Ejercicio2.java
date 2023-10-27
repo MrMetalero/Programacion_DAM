3ebcdc04f0d18f9a47cecd278dc7418ae3563f7d
MrMetalero <danymena7@gmail.com>
2023-10-27 10:45:03 +0200

    update launch.json and Ejercicio2.java

--- ejercicios_propuestos_t11/Ejercicio2.java
+++ ejercicios_propuestos_t11/Ejercicio2.java
@@ -1,88 +1,68 @@
 package ejercicios_propuestos_t11;
+
 import java.util.Scanner;
 
 public class Ejercicio2 {
-    
 
-    public static int funcionFibonacciIterativo(int inputNumber){
+    public static int funcionFibonacciIterativo(int inputNumber) {
         int numSucessionAnterior2 = 0;
         int numSucessionAnterior1 = 0;
-        
+
         int numeroSucesionSiguiente = 0;
 
-        
-        for(int i = 0; i < inputNumber; i++){
-           
+        for (int i = 0; i < inputNumber; i++) {
 
-            
-            if(i > 1){
-            numeroSucesionSiguiente =  numSucessionAnterior2 + numSucessionAnterior1; //Al inicio 1+0 = 1
-            System.out.print(numeroSucesionSiguiente + ", ");  //Printeo ese primer valor (1)
-            numSucessionAnterior2 = numSucessionAnterior1; // meto el valor que antes era el PRIMER ANTERIOR como el SEGUNDO ANTERIOR
-            numSucessionAnterior1 = numeroSucesionSiguiente; // meto el valor que antes era el RESULTADO en el PRIMER ANTERIOR
+            if (i > 1) {
+                numeroSucesionSiguiente = numSucessionAnterior2 + numSucessionAnterior1; // Al inicio 1+0 = 1
+                System.out.print(numeroSucesionSiguiente + ", "); // Printeo ese primer valor (1)
+                numSucessionAnterior2 = numSucessionAnterior1; // meto el valor que antes era el PRIMER ANTERIOR como el
+                                                               // SEGUNDO ANTERIOR
+                numSucessionAnterior1 = numeroSucesionSiguiente; // meto el valor que antes era el RESULTADO en el
+                                                                 // PRIMER ANTERIOR
 
-             
+            } else {
 
-            } 
-            else{
-
-                if(i == 0){
+                if (i == 0) {
                     numeroSucesionSiguiente = numSucessionAnterior1 + numSucessionAnterior2;
                     System.out.print(numeroSucesionSiguiente + ", ");
                     numSucessionAnterior1 += 1;
 
-                }
-                else{
+                } else {
                     numeroSucesionSiguiente = numSucessionAnterior1 + numSucessionAnterior2;
                     System.out.print(numeroSucesionSiguiente + ", ");
                 }
-                
 
             }
 
-
         }
-
 
         return 0;
     }
 
+    public static int funcionRecursivaFibonacci(int limiteFibonacci, int numeroIzquierda2, int numeroIzquierda1,
+            int conteo) {
 
-
-    public static int funcionRecursivaFibonacci(int limiteFibonacci, int numeroIzquierda2, int numeroIzquierda1){
-
-        //Caso base
-        // fibonacci_x(10) → 0, 1, 1,                      2, 3, 5, 8, 13, 21, 34
-        //       (0)                (1)
+        // Caso base
+        // fibonacci_x(10) → 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+        // (0) (1)
         // numeroIzquierda2 + numeroIzquierda1 = 1
-         numeroIzquierda2 = 0;  numeroIzquierda1 = 1;
+
         int numeroSiguiente;
-        
 
-        if (numeroIzquierda2 + numeroIzquierda1 != 1) {
-            
-            funcionRecursivaFibonacci()
+        if (limiteFibonacci != conteo) {
+            numeroSiguiente = numeroIzquierda2 + numeroIzquierda1;
+            System.out.print(numeroIzquierda2 + " | ");
+            System.out.println(numeroSiguiente);
 
+            numeroIzquierda2 = numeroIzquierda1;
+            numeroIzquierda1 = numeroSiguiente;
 
+            funcionRecursivaFibonacci(limiteFibonacci, numeroIzquierda2, numeroIzquierda1, conteo += 1);
 
-        } else {
-            System.out.print(numeroSiguiente);
         }
-
-
-
-
 
         return 0;
     }
-
-
-
-
-
-
-
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -91,6 +71,9 @@ public class Ejercicio2 {
         int numInputFibonacci = sc.nextInt();
 
         funcionFibonacciIterativo(numInputFibonacci);
+        System.out.println("\n");
+
+        funcionRecursivaFibonacci(numInputFibonacci, 0, 1, 0);
 
     }
 }
