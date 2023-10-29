2af4257606b64136a4c8ca3455fcb7069cc2aa3f
Mr_Metalero <danymena7@gmail.com>
2023-10-29 21:06:14 +0100

    dadqwd1qd

--- ejercicios_pag213y214/Ejercicio5.java
+++ ejercicios_pag213y214/Ejercicio5.java
@@ -1,11 +1,54 @@
 package ejercicios_pag213y214;
+import java.util.Scanner;
 
 public class Ejercicio5 {
     
-    public static void main(String[] args) {
+
+    public static int funcionDivisionRestas(int dividendo, int divisor, int resultado){
+
+        if(dividendo - divisor == 0 ){
+            
+            return resultado+1;
+
+
+        }else{
+            
+            return funcionDivisionRestas(dividendo-divisor, divisor, resultado+1);
+        }
+
         
+
+    }
+
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Introduce el dividendo");
+        int dividendo = sc.nextInt();
+        System.out.println("Introduce el divisor");
+        int divisor = sc.nextInt();
+
+
+        
+        while(dividendo < divisor){
+            System.out.println("El divisor tiene que ser menor que el dividendo");
+            System.out.println("Introduce el dividendo");
+            dividendo = sc.nextInt();
+            System.out.println("Introduce el divisor");
+            divisor =sc.nextInt();
+
+
+        }
+
+        
+        System.out.println("El resultado de la división es : "+ funcionDivisionRestas(dividendo, divisor, 0));
+
+
+
 
 
     }
-    
+
 }
