30b4eb62778cad69eb087c54f50007f9ee326e78
Mr_Metalero <danymena7@gmail.com>
2023-11-08 22:10:10 +0100

    arrays y cositas

--- actividades_propuestas_t8/Ejercicio3.java
+++ actividades_propuestas_t8/Ejercicio3.java
@@ -10,27 +10,118 @@ public class Ejercicio3 {
         System.out.println("Introduce el número de alumnos que vas a introducir");
         int limiteArray = sc.nextInt();
 
+        //creamos el array
         String[] alumnosArray = new String[limiteArray];
-        int controlAlumnos;
-        String nombreCache = "";
+        String Basura = ""; //recoge las putas lineas tocahuevos PD: Introducir lineas por aquí es una mierda
 
-        controlAlumnos = alumnosArray.length;
+        for (int i = 0; i < alumnosArray.length; i++) { //recorre los indices del array para introducir nombres
 
-        for (int i = 0; i < controlAlumnos; i++) {
+            System.out.println("Introduce el alumno: " + i +" ");
+            
+            Basura = sc.nextLine(); //recoge las líneas que haya en el buffer para que no nos fastidie el input
+            alumnosArray[i] = sc.next();
+            
+        }
 
-            System.out.println("Introduce el alumno: " + i+" ");
-            sc.next();
 
-            nombreCache = sc.nextLine();
-            alumnosArray[i] = nombreCache;
+
+        //solo para listar el Array con los indices
+        for (int i = 0; i < alumnosArray.length; i++) {
+
+            System.out.println("["+ i+"] " + alumnosArray[i] );
+            
            
         }
+        System.out.println("El array contiene los valores:" + Arrays.toString(alumnosArray));
         
 
-        System.out.println("El array contiene los valores:" + alumnosArray[0]);
+
+        //Menú--------------------------------------
+
+
+
+
+
+
         
+      
+        int controlMenu = 50;
+
+        //Es posible que los necesite ordenados así que haremos eso mismo con los elementos del array con esto
+        
+
+        //Menú para las diferentes opciones, preguntará siempre al acabar una operación hast que metas 0
+        while(controlMenu != 0){
+            System.out.println("Introduce 1 para eliminar a un alumno del array");
+            System.out.println("Introduce 2 para añadir un alumno al array");
+            System.out.println("Introduce 3 para ordenar el array");
+            System.out.println("Introduce 0 para finalizar el programa");
+
+            controlMenu = sc.nextInt(); //Seleccionas la opción del menú
+
+
+
+            if(controlMenu == 1){
+
+
+                System.out.println("introduce el nombre a borrar");
+                Basura = sc.nextLine();
+
+                String nombreBorrar = sc.next();
+                int nombreEncontradoEn = 0;
+
+                for (int i = 0; i < alumnosArray.length; i++) {
+                    
+                    if(alumnosArray[i].equals(nombreBorrar)){
+
+                        alumnosArray[i] = "";
+
+                    }
+
+                }
+
+                //solo para listar el Array con los indices
+                for (int i = 0; i < alumnosArray.length; i++) {
+
+                    System.out.println("["+ i +"] " + alumnosArray[i] );
+                    
+                
+                }
+               
+
+            }
+            else if (controlMenu == 2){
+
+
+
+
+
+
+            }
+            else if (controlMenu == 3){
+
+                Arrays.sort(alumnosArray);
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
 
     }
+
+
+
+
+
+
 
 }
 
