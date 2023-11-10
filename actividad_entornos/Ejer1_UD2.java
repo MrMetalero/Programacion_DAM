278d3609e25f3e97ae1da28be678653c3de565eb
Mr_Metalero <danymena7@gmail.com>
2023-11-10 17:33:06 +0100

    dadada

---
+++ actividad_entornos/Ejer1_UD2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad_entornos;
+/**
+ * @author Dani
+ */
+public class Ejer1_UD2 {
+
+//Código original:
+
+
+    /* 
+        public static void main(String[] args) {
+            
+            int i, j, z, filas=6;
+            
+            for (j = 1; i <= filas; i++){        //Punto de ruptura recomendable
+                if ((j == 1) || (i == filas))        //Punto de ruptura recomendable
+                    for (i = 1; j <= filas; j++)       //Punto de ruptura recomendable
+                        System.out.print("");
+                else
+                    System.out.println("*");
+            }
+            
+            
+        }
+    
+
+
+        // A continuación como he dejdo el código para arreglarlo.
+        // He usado la herramienta de debug con la opción de entrar en profundidad
+        // paso a paso en Visual Studio, para visualizar el proceso.  F11 (Por instrucciones)
+        // F10 para ir paso a paso por procedimientos
+
+    */
+
+    public static void main(String[] args) {
+     
+        int i, j = 0, z, filas=6;  // z no se usa así que lo borraría
+       
+        for (i = 0; i <= filas; i++){   //Punto de ruptura recomendable
+
+            if ((j == 1) || (i == filas)){  //punto de ruptura recomendable
+
+                for (j = 1; j <= filas; j++){ //punto de ruptura recomendable
+                    System.out.print("");
+                }
+
+            }
+            else{
+                System.out.println("*");
+            }
+                
+        }
+    }    
+        
+}
+   
---
+++ actividad_entornos2/Ejer2_UD2.java
@@ -0,0 +1,27 @@
+
+package actividad_entornos2;
+
+
+public class Ejer2_UD2 {
+
+    
+public static void main(String[] args) {
+    
+int i, j, k;
+for (k = 1; i <= 3; i++) { //bucle que cuenta hasta 10
+for (i = 1; i <= 3-k; i++) {
+System.out.print( " ");
+}
+for (j = 1; j <= 2 * (j-1)+ 1; j++) {
+System.out.print( "*");
+}
+System.out.println( ".");
+}
+}
+
+
+
+
+}
+
+
