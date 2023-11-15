7ea8f56e01798ec3e25b5877b89cd7f6dd65899b
MrMetalero <danymena7@gmail.com>
2023-11-15 14:47:16 +0100

    aafafafa

--- ejercicios_practicos_t8/Ejercicio5.java
+++ ejercicios_practicos_t8/Ejercicio5.java
@@ -1,14 +1,38 @@
 package ejercicios_practicos_t8;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Ejercicio5 {
-    
+
+    public static int solicitarSize() {
+        Scanner scSize = new Scanner(System.in);
+
+        System.out.println("Introduce la longitud del Vector a crear");
+        int longitudVector = scSize.nextInt();
+        scSize.close();
+
+        return longitudVector;
+    }
+
+    public static int[] asignarDatos(int[] arrayInput) {
+
+        // Random-------------------------------------------
+
+        for (int i = 0; i < arrayInput.length - 1; i++) {
+            arrayInput[i] = (int) (Math.random() * 9999999);
+
+        }
+
+        return arrayInput;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int lon = solicitarSize();
 
-
-
-
-        
+        int[] v = new int[lon];
+        v = asignarDatos(v);
 
     }
 
--- supuestos_practicos_arraysT8/Ejercicio2.java
+++ supuestos_practicos_arraysT8/Ejercicio2.java
@@ -1,86 +1,73 @@
 package supuestos_practicos_arraysT8;
+
 import java.util.Scanner;
 
 public class Ejercicio2 {
-    
-    public static String funcionComprobarString(String[] stringInput, String numeroDNI){
+
+    public static String funcionComprobarString(String[] stringInput, String numeroDNI) {
         String resultado = "";
         String numeroArrayEntero = "";
         int restoDivision = 0;
 
-        
-
-        
-        if(numeroDNI.length() != 9){
+        if (numeroDNI.length() != 9) {
             resultado += " |DNI no válido por longitud";
-        }
-        else if(String.valueOf(numeroDNI.charAt(numeroDNI.length()-1)).matches("[^TRWAGMYFPDXBNJZSQVHLCKEa-z]") ){ //comprueba que la letra sea posible
+        } else if (String.valueOf(numeroDNI.charAt(numeroDNI.length() - 1)).matches("[^TRWAGMYFPDXBNJZSQVHLCKEa-z]")) { // comprueba
+                                                                                                                        // que
+                                                                                                                        // la
+                                                                                                                        // letra
+                                                                                                                        // sea
+                                                                                                                        // posible
 
             resultado += " |DNI No Valido por letra no existe";
         }
-        
-        for(int i = 0; i < numeroDNI.length()-1; i++){ //Pasa por los valores de numeroDNI y los suma a numeroArrayEntero
+
+        for (int i = 0; i < numeroDNI.length() - 1; i++) { // Pasa por los valores de numeroDNI y los suma a
+                                                           // numeroArrayEntero
             numeroArrayEntero = numeroArrayEntero + numeroDNI.charAt(i);
-            
-            
 
-           
         }
-        
 
+        // Este paso de división se hace así porque el algoritmo de asignación del
+        // gobierno asigna la letra dependiendo del resto de
+        // la división del número del DNI entre 23
+        restoDivision = Integer.valueOf(numeroArrayEntero) % 23; // divide entre 23 el valor de numeroArrayEntero sin la
+                                                                 // letra, guarda el resto
 
-        //Este paso de división se hace así porque el algoritmo de asignación del gobierno asigna la letra dependiendo del resto de
-        //la división del número del DNI entre 23
-        restoDivision = Integer.valueOf(numeroArrayEntero) % 23; //divide entre 23 el valor de numeroArrayEntero sin la letra, guarda el resto
-        
-      
-        //Para limpiar un poco la condición de debajo y hacerla más legible he usado estas variables para guardar
+        // Para limpiar un poco la condición de debajo y hacerla más legible he usado
+        // estas variables para guardar
 
-        // en la primera el valor de la letra correspondiente al array con índice en la posicion restoDivision
+        // en la primera el valor de la letra correspondiente al array con índice en la
+        // posicion restoDivision
         String valorIndice = String.valueOf(stringInput[restoDivision]);
-        //en este segundo se guarda el valor de la letra contenida en el DNI que hemos pasado como input
-        String valorDNILetra = String.valueOf(numeroDNI.charAt(numeroDNI.length()-1));
+        // en este segundo se guarda el valor de la letra contenida en el DNI que hemos
+        // pasado como input
+        String valorDNILetra = String.valueOf(numeroDNI.charAt(numeroDNI.length() - 1));
 
+        // Si el valor de la letra asignada al (índice del array correspondiente al
+        // resto) NO es igual a la letra del DNI que aportamos
 
-
-        //Si el valor de la letra asignada al (índice del array correspondiente al resto) NO es igual a la letra del DNI que aportamos
-        
-        if(valorIndice.equals(valorDNILetra)){
+        if (valorIndice.equals(valorDNILetra)) {
             resultado += " |Valido";
 
-        }else{
+        } else {
             resultado += " |Letra incorrecta";
         }
 
-        
         return resultado;
 
-        
     }
-    
-
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arrayLetras = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+        String[] arrayLetras = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q",
+                "V", "H", "L", "C", "K", "E" };
         String dniValidar;
-
 
         System.out.println("Introduce un DNI para validar con la LETRA MAYÚSCULA: ");
         dniValidar = sc.nextLine();
-        
+
         System.out.println(funcionComprobarString(arrayLetras, dniValidar));
 
-
-
-
-      
-
-
-
     }
-
-
 
 }
