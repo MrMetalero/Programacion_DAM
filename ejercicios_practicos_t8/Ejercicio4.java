e9044fe07636a0a28df494e71610e3deb217d8a1
MrMetalero <danymena7@gmail.com>
2023-11-15 14:14:03 +0100

    afafaa

--- ejercicios_practicos_t8/Ejercicio4.java
+++ ejercicios_practicos_t8/Ejercicio4.java
@@ -1,14 +1,15 @@
 package ejercicios_practicos_t8;
+
 import java.util.Scanner;
 
 public class Ejercicio4 {
-    
+
     public static void main(String[] args) {
-        
+
         int[][] matrizNumeros = new int[4][4];
         Scanner sc = new Scanner(System.in);
         int sumaTotal = 0;
-        int mediaTotal = 0;
+        double mediaTotal = 0;
         int sumaDiagonal = 0;
         int sumaDiagonalInversa = 0;
 
@@ -17,21 +18,19 @@ public class Ejercicio4 {
         int controlColumna = 0;
         int sumaColumna = 0;
 
-
-        //Asignar números a la matriz
+        // Asignar números a la matriz
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
 
-                System.out.println("Introduce un valor para la posición fila: " + i + " columna " + j );
+                System.out.println("Introduce un valor para la posición fila: " + i + " columna " + j);
                 matrizNumeros[i][j] = sc.nextInt();
                 sumaTotal += matrizNumeros[i][j];
             }
 
-
         }
 
-        //sumar una fila concreta
+        // sumar una fila concreta
 
         System.out.println("Introduce la fila a sumar");
         controlFila = sc.nextInt();
@@ -40,23 +39,19 @@ public class Ejercicio4 {
             sumaFila += matrizNumeros[controlFila][i];
         }
 
-
-        //sumar una columna concreta
+        // sumar una columna concreta
 
         System.out.println("Introduce la columna a sumar");
         controlColumna = sc.nextInt();
-        
+
         for (int i = 0; i < 3; i++) {
             sumaColumna += matrizNumeros[i][controlColumna];
         }
 
+        // calcular la media
+        mediaTotal = sumaTotal / 16;
 
-        //calcular la media
-        mediaTotal = sumaTotal/16;
-
-
-
-        //Suma de la diagonal y la diagonal inversa
+        // Suma de la diagonal y la diagonal inversa
         for (int i = 0; i < 3; i++) {
             sumaDiagonal += matrizNumeros[i][i];
 
@@ -67,25 +62,15 @@ public class Ejercicio4 {
 
         }
 
+        // Imprimir por pantalla todo
 
-        //Imprimir por pantalla todo
-        
-        System.out.println("suma Total: "+ sumaTotal);
-        System.out.println("Media: "+ mediaTotal);
-        System.out.println("suma Diagonal: "+ sumaDiagonal);
-        System.out.println("suma Diagonal Inversa: "+ sumaDiagonalInversa);
-        System.out.println("suma Columna: "+ sumaColumna);
-        System.out.println("suma Fila: "+ sumaFila);
-        
-
-        
-
-
-
-
-
+        System.out.println("suma Total: " + sumaTotal);
+        System.out.println("Media: " + mediaTotal);
+        System.out.println("suma Diagonal: " + sumaDiagonal);
+        System.out.println("suma Diagonal Inversa: " + sumaDiagonalInversa);
+        System.out.println("suma Columna: " + sumaColumna);
+        System.out.println("suma Fila: " + sumaFila);
 
     }
-
 
 }
---
+++ ejercicios_practicos_t8/Ejercicio5.java
@@ -0,0 +1,15 @@
+package ejercicios_practicos_t8;
+
+public class Ejercicio5 {
+    
+    public static void main(String[] args) {
+        
+
+
+
+
+        
+
+    }
+
+}
