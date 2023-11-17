5a361ee66da3f191b5b3790eb152a3351061e72c
MrMetalero <danymena7@gmail.com>
2023-11-17 14:00:27 +0100

    dafafaf

---
+++ ejercicios_practicos_t8/Ejercicio6.java
@@ -0,0 +1,22 @@
+package ejercicios_practicos_t8;
+
+public class Ejercicio6 {
+
+
+    public class Cadenas{
+        String cad1;
+        String cad2;
+        String cad3;
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        String cad1 = "Pepe";
+        String cad2 = new String("Lionel");
+        String cad3 = new String(cad2);
+
+    }
+
+}
