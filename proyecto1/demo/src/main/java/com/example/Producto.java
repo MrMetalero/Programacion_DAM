f1066afed00dc6ab44662a0d72176c049a04ef0a
MrMetalero <danymena7@gmail.com>
2023-11-29 14:53:57 +0100

    cambiado para clases

---
+++ proyecto1/demo/src/main/java/com/example/Producto.java
@@ -0,0 +1,18 @@
+package proyecto1.demo.src.main.java.com.example;
+
+public class Producto {
+    
+   float precio;
+
+
+
+    public float infoRebajaActual(){
+        
+
+
+        return
+    }
+
+
+
+}
