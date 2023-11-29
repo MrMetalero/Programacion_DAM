7c58653d96da1000205469a97d9c9118eb1ee7ad
MrMetalero <danymena7@gmail.com>
2023-11-29 13:53:15 +0100

    nuevo proyecto

---
+++ proyecto1/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package proyecto1.demo.src.main.java.com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+        
+    }
+}
