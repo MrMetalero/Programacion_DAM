9987bb4ebc74b4cbbf2c5a590ce5981716790484
MrMetalero <danymena7@gmail.com>
2023-12-12 12:52:52 +0100

    ajkdkdadnadkadkl cambios

--- ejercicios_adicionales_objetos/demo/src/main/java/com/example/Main.java
+++ ejercicios_adicionales_objetos/demo/src/main/java/com/example/Main.java
@@ -1,7 +1,35 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el nombre");
+        String nombre = sc.nextLine();
+
+        System.out.println("Introduce el edad");
+        String edad = sc.nextLine();
+
+        System.out.println("Introduce el edad");
+        char sexo = sc.next().charAt(0);
+
+        System.out.println("Introduce el peso");
+        int peso = sc.nextInt();
+
+        System.out.println("Introduce el altura");
+        float altura = sc.nextFloat();
+
+
+
+        Persona Dan = new Persona(nombre,edad,peso,altura);
+
+
+
+
+
+
     }
 }
--- ejercicios_adicionales_objetos/demo/src/main/java/com/example/Persona.java
+++ ejercicios_adicionales_objetos/demo/src/main/java/com/example/Persona.java
@@ -9,7 +9,7 @@ public class Persona {
     int peso = 0;
     float altura = 0;
 
-
+    int contadorPersonas;
 
 
     public Persona(){
@@ -19,21 +19,23 @@ public class Persona {
         this.peso = peso;
         this.altura = altura;
 
-        
+        contadorPersonas += 1;
     }
 
   
 
 
     public Persona(String nombre,int edad,char sexo){
-        if(sexo == 'H' && sexo == 'M' && edad > 0 && nombre != "" ){
+        
+        comprobarSexo(sexo);
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
 
-            this.nombre = nombre;
-            this.edad = edad;
-            this.peso = peso;
-            this.altura = altura;
+        contadorPersonas += 1;
 
-        }else{System.out.println("Datos incorrectos");}
+    
 
         
 
@@ -46,11 +48,166 @@ public class Persona {
         this.peso = peso;
         this.altura = altura;
 
+        contadorPersonas += 1;
+    
+    }
+
+    public float calcularIMC(){
+        float imcCalculado;
+        float resultado = 0;
+
+        imcCalculado = peso/(altura*altura);
+
+        if (imcCalculado < 20){
+            resultado = -1;
+
+        }else 
+            if (imcCalculado >= 20 && imcCalculado <= 25){
+                resultado = 0;
+            }
+            else 
+                if(imcCalculado > 25){
+                    resultado = 1;
+                }
+        
+
+        return resultado;
+    }  
+
+    public String devuelveString(){
+        String devueltoString = "Paciente: "+ nombre + "\n"
+                                +"Edad: "+ edad + "años \n"
+                                +"Sexo: "+ sexo + "\n"
+                                +"Peso: " + peso + " Kg\n"
+                                +"Altura: "+ altura + " m\n"
+        ;
+
+
+        return devueltoString;
+    }
+    
+
+    public boolean esMayorDeEdad(){
+        boolean mayorEdad = false;
+
+        if (edad > 18){
+            mayorEdad = true;
+
+        }
+
+
+        return mayorEdad;
+    }
+
+    //Package private comprobarSexo
+
+    private void comprobarSexo(char sexo){
+
+        if (sexo == 'H' || sexo == 'M'){
+            
+
+        }else{
+            sexo = sexoFinal;
+        }
+
     
     }
 
 
-    
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+
+
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+
+
+    public char getSexo() {
+        return sexo;
+    }
+
+
+
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+
+
+
+    public static char getSexofinal() {
+        return sexoFinal;
+    }
+
+
+
+
+    public int getPeso() {
+        return peso;
+    }
+
+
+
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+
+
+
+    public float getAltura() {
+        return altura;
+    }
+
+
+
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+
+
+
+    public int getContadorPersonas() {
+        return contadorPersonas;
+    }
+
+
+
+
+    public void setContadorPersonas(int contadorPersonas) {
+        this.contadorPersonas = contadorPersonas;
+    }
+
+
+
+
+
 
 
 }
