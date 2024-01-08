08b3ac4812a77bc7d6573f1fd7d0806185def492
MrMetalero <danymena7@gmail.com>
2024-01-08 14:52:41 +0100

    fafafakfakfaf

--- ejercicios_practicos_t7/ejercicio1/demo/src/main/java/com/example/Main.java
+++ ejercicios_practicos_t7/ejercicio1/demo/src/main/java/com/example/Main.java
@@ -1,38 +1,25 @@
 package com.example;
 
-
-
-
-
-
-
-
-
-
-
-
 public class Main {
     public static void main(String[] args) {
-      
-        Pelicula pelicula1 = new Pelicula("Interestellar", "Christopher Nolan", Formato.avi, 190,"Mathew McConaughey","Anne Hathaway");
 
-        Pelicula pelicula2 = new Pelicula("Interestellar", "Christopher Nolan", Formato.avi, 190,"Mathew McConaughey","Anne Hathaway");
+        Pelicula pelicula1 = new Pelicula("Interestellar", "Christopher Nolan", Formato.avi, 190, "Mathew McConaughey",
+                "Anne Hathaway");
 
-        Pelicula pelicula3 = new Pelicula("Django", "Tarantino", Formato.dvd, 120, "Mipadre","");
+        Pelicula pelicula2 = new Pelicula("Interestellar", "Christopher Nolan", Formato.avi, 190, "Mathew McConaughey",
+                "Anne Hathaway");
+
+        Pelicula pelicula3 = new Pelicula("Django", "Tarantino", Formato.dvd, 120, "Mipadre", "");
 
         System.out.println("Comprobando las películas...");
 
-        if(pelicula1.equals(pelicula2)){
+        if (pelicula1.equals(pelicula2)) {
             System.out.println("Las películas son iguales");
 
-
-        }else {
+        } else {
             System.out.println("Las películas son diferentes");
 
         }
 
-
-
-        
     }
 }
