d7b09a6332426d2fd287f6c8d9e3889da5ce36df
MrMetalero <danymena7@gmail.com>
2024-01-11 10:23:48 +0100

    cambios en congelados etc

--- ejercicios_practicos_t7/ejercicio4/demo/src/main/java/com/example/Main.java
+++ ejercicios_practicos_t7/ejercicio4/demo/src/main/java/com/example/Main.java
@@ -1,10 +1,22 @@
 package com.example;
 
+import com.example.Congelados.CongeladosAgua;
+import com.example.Congelados.CongeladosAire;
+
 public class Main {
     public static void main(String[] args) {
 
-        Refrigerado refri1 = new Refrigerado("17/2/2056", 12313, "23/5/1923", "España", "C23fadada", 1029);
+        Refrigerado refri1 = new Refrigerado("17/02/2056", 12313, "23/05/1923", "España", "C23fadada", 1029);
 
         System.out.println(refri1.toString());
+
+
+        CongeladosAgua conge1 = new CongeladosAgua("17/05/2822", 12415, "29/06/2033", "Finlandia", -2, 23);
+
+        System.out.println(conge1.toString());
+
+        CongeladosAire congeA1 = new CongeladosAire("12/02/2034", 252514, "10/02/2033", "Afghanistan", 2, 10, 70, 10, 10);
+
+
     }
 }
