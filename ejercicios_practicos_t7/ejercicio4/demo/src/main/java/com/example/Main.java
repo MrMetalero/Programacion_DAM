f4dbf23754b0a72884b990697460da7ce358ac48
MrMetalero <danymena7@gmail.com>
2024-01-11 13:02:07 +0100

    jejej

--- ejercicios_practicos_t7/ejercicio4/demo/src/main/java/com/example/Main.java
+++ ejercicios_practicos_t7/ejercicio4/demo/src/main/java/com/example/Main.java
@@ -2,11 +2,12 @@ package com.example;
 
 import com.example.Congelados.CongeladosAgua;
 import com.example.Congelados.CongeladosAire;
+import com.example.Congelados.CongeladosNitrogeno;
 
 public class Main {
     public static void main(String[] args) {
 
-        Refrigerado refri1 = new Refrigerado("17/02/2056", 12313, "23/05/1923", "España", "C23fadada", 1029);
+        Refrigerado refri1 = new Refrigerado("17/02/2056", 12313, "23/05/1923", "España", "C23fadada", 1);
 
         System.out.println(refri1.toString());
 
@@ -15,8 +16,19 @@ public class Main {
 
         System.out.println(conge1.toString());
 
-        CongeladosAire congeA1 = new CongeladosAire("12/02/2034", 252514, "10/02/2033", "Afghanistan", 2, 10, 70, 10, 10);
 
+        
+        try{
+        CongeladosAire congeA1 = new CongeladosAire("12/02/2034", 252514, "10/02/2033", "Afghanistan", 2, 10, 70, 10, 10);
+            System.out.println(congeA1.toString());
+
+        }catch(Exception Exception){
+            
+        }
+        
+
+        CongeladosNitrogeno congeNitro1 = new CongeladosNitrogeno("12/06/2033", 2425252, "19/09/9233", "Venezuela", 1, "Método 1", 4500);
+        System.out.println(congeNitro1.toString());
 
     }
 }
