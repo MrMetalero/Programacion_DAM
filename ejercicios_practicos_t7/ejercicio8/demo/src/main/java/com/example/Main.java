16758cf4d33544cfb1c272d802ee67626a07b802
MrMetalero <danymena7@gmail.com>
2024-01-12 14:01:32 +0100

    Sus muertos con bases de datos colega

---
+++ ejercicios_practicos_t7/ejercicio8/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+class prueba {
+    protected String nombre;
+    protected int ID;
+
+    public String getIdent() {
+        return nombre;
+    }
+
+    public int getIdent() {
+        return ID;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
