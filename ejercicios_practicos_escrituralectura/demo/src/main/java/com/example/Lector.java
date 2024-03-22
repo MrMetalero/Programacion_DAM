d6e8ca909d7fde04563234d9e043a0bd200a28c8
MrMetalero <danymena7@gmail.com>
2024-03-22 12:51:35 +0100

    ejercicios Ficheros

---
+++ ejercicios_practicos_escrituralectura/demo/src/main/java/com/example/Lector.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Lector{
+    
+    static int i;
+
+    
+    public static void modificarFichero(FileReader ficheroInput) throws FileNotFoundException ,IOException {
+        
+
+        
+
+        
+    }
+    
+  
+
+
+     
+
+
+}
--- ejercicios_practicos_escrituralectura/demo/src/main/java/com/example/Main.java
+++ ejercicios_practicos_escrituralectura/demo/src/main/java/com/example/Main.java
@@ -1,60 +1,49 @@
 package com.example;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.charset.Charset;
 
 public class Main {
     public static void main(String[] args) {
        
+        int i;
+        String textoCache = null;
+        try {
+            FileReader fr = new FileReader("demo/src/main/resources/texto.txt");
+            BufferedReader br = new BufferedReader(fr); //Lector Buffer
 
-            File fichero = new File("/resources/texto.txt");
 
-            try {
-                FileReader lector = new FileReader(fichero);
-            } catch (FileNotFoundException e) {
-            
-                e.printStackTrace(); //Error archivo no encontrado
-            }
+            FileWriter fw = new FileWriter("demo/src/main/resources/texto.txt");
+            BufferedWriter bw = new BufferedWriter(fw); // Escritor Buffer
 
-            FileInputStream fileInStr;
-            try {
-                fileInStr = new FileInputStream(fichero);
-            } catch (FileNotFoundException e) {
-                
-                e.printStackTrace(); //Error archivo no encontrado
-            }
-            BufferedInputStream bInputStr = new BufferedInputStream(fileInStr);
-            
-            int i;
-
-            try {
-                while ((i=bInputStr.read()) != -1) {
-                    System.out.println((char)i);
+            Lector.modificarFichero(fr);
+            while (br.ready()) {
+                if ((char) br.read() == ' ') {
+                    
                 }
-            } catch (IOException e) {
             
-                e.printStackTrace();
-            }
-
-            try {
-                bInputStr.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            try {
-                fileInStr.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
 
 
+        } catch(FileNotFoundException e) {
+            System.out.println(e.getMessage());
+
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        
+        
+        
+        
 
     }
 }
