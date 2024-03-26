cfb42b9f8ad6f1da3ad219b407e7fd1a103f3efa
MrMetalero <danymena7@gmail.com>
2024-03-26 11:39:54 +0100

    mensaje uwu

--- ejercicios_practicos_escrituralectura/Ejercicio1/demo/src/main/java/com/example/Main.java
+++ ejercicios_practicos_escrituralectura/Ejercicio1/demo/src/main/java/com/example/Main.java
@@ -16,23 +16,23 @@ public class Main {
     public static void main(String[] args) {
        
         int i;
-        String textoCache = null;
+        String textoCache = "";
         try {
             FileReader fr = new FileReader("demo/src/main/resources/texto.txt");
             BufferedReader br = new BufferedReader(fr); //Lector Buffer
 
 
-            FileWriter fw = new FileWriter("demo/src/main/resources/texto.txt");
-            BufferedWriter bw = new BufferedWriter(fw); // Escritor Buffer
-
-            Lector.modificarFichero(fr);
-            while (br.ready()) {
-                if ((char) br.read() == ' ') {
-                    
-                }
             
-            }
+            while ((i=br.read())!= -1) {
 
+                if ((char)i != ' '){
+                textoCache  += (char)i;
+                }
+                
+                System.out.println(textoCache);
+            }
+        br.close(); // necesarios para que se efectue la lectura correctamente ??
+        fr.close();
 
         } catch(FileNotFoundException e) {
             System.out.println(e.getMessage());
@@ -40,6 +40,21 @@ public class Main {
         } catch(IOException e){
             System.out.println(e.getMessage());
         }
+        
+
+        FileWriter fw;
+        try {
+            fw = new FileWriter("demo/src/main/resources/texto.txt");
+            BufferedWriter bw = new BufferedWriter(fw); // Escritor Buffer
+
+            bw.write(textoCache);
+            bw.close(); // necesarios para que se efectue la escritura correctamente
+            fw.close(); // si no se ponen no se guarda nada en el archivo
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+        
         
         
         
