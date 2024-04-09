5e4d6dd4c2367ef9cb1e4ecb1a5706eb642790a0
MrMetalero <danymena7@gmail.com>
2024-04-09 13:05:31 +0200

    ejercicio 6

--- ejercicios_practicos_escrituralectura/Ejercicio6/src/App.java
+++ ejercicios_practicos_escrituralectura/Ejercicio6/src/App.java
@@ -1,4 +1,7 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,9 +9,13 @@ public class App {
         
         Vehiculo vehiculo1 = new Vehiculo("13121", "Seat", "Leon", 123.0)
 
-
-        DataInputStream
-
+        FileInputStream fis = new FileInputStream("resources/vehiculos.txt");
+        DataInputStream dis = new DataInputStream(fis);
+        
+        FileOutputStream fos = new FileOutputStream("resources/vehiculos.txt");
+        DataOutputStream dos = new DataOutputStream(fos);
+        
+        
 
 
     }
